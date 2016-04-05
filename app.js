6d1aae3fe91e179b070eae18c22f7044053c10c6
Peter Colaric <pc2612@student.uni-lj.si>
2016-04-05 17:28:13 +0000

    Končna oddaja

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ if (!process.env.PORT)
 /* initialization of Chinook database */
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('chinook.sl3');
-
+ var perpages = 20;
 /* calls callback with specified page's artists and artist's details */
 var artists = function(page, artist, details, callback) {
   db.all("SELECT Artist.ArtistId, Name, StarsNo " +
     "FROM Artist, Stars WHERE Artist.ArtistId = Stars.ArtistId " +
-    "ORDER BY Name LIMIT 33 OFFSET ($page - 1) * 33",
+    "ORDER BY Name LIMIT 20 OFFSET ($page - 1) * 20",
     {$page: page}, function(error, rows) {
       if (error) {
         console.log(error);
@@ -18,7 +18,7 @@ var artists = function(page, artist, details, callback) {
         var result = '<div id="artists">';
         for (var i = 0; i < rows.length; i++) {
           var selected = rows[i].ArtistId == artist;
-          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * 33 + i - 32) + '.</span>' +
+          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * perpages + i - perpages+1) + '.</span>' +
             '<a href="/artists/' + page + (!selected? '/details/' + rows[i].ArtistId: '') + '#' + rows[i].ArtistId + '">' +
             '<button type="button" class="btn btn-default' + (selected? ' selected': '') + '">' +
             rows[i].Name + '</button></a><span class="stars">';
@@ -93,24 +93,41 @@ var genres = function(artist, callback) {
         console.log(error);
         callback('<strong>Something went wrong!</strong>');
       } else {
-        var result = '<h5>Genres</h5><div id="genres">' + 
-          'No genres for this artist' + 
-          '</div>';
+        var result = "";
+        if(rows.length != 0){
+            result += '<h5>Genres</h5><div id="genres">';
+            for(var i = 0; i < rows.length; i++){
+              result += rows[i].Name;
+              if(i != (rows.length -1)) result += " | ";
+            }
+            result += '</div>';
+          
+        }else{
+          result = '<h5>Genres</h5><div id="genres">' + 
+            'No genres for this artist' + 
+            '</div>';
+        }
         callback(result);
       }
   });
 }
 
+//Make server
 /* initialization of Express application */
 var express = require('express');
 var app = express();
-
+//Serving static files
 /* settings for static application files */
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 /* responds with first page's artists */
 app.get('/artists', function(request, response) {
+  response.redirect('/artists/1');
+});
+
+/* responds with first page's artists */
+app.get('/', function(request, response) {
   response.redirect('/artists/1');
 });
 
@@ -179,7 +196,7 @@ app.get('/stars/:artist/:stars', function(request, response) {
         response.send({artist: request.params.artist, stars: request.params.stars});
   });
 });
-
+ 
 /* responds with number of artist pages */
 app.get('/pages', function(request, response) {
   db.get("SELECT COUNT(*) AS Artists FROM Artist", function(error, row) {
@@ -187,8 +204,13 @@ app.get('/pages', function(request, response) {
       console.log(error);
       response.sendStatus(500);
     } else
-      response.send({pages: Math.ceil(row.Artists / 33)});
+      response.send({pages: Math.ceil(row.Artists / perpages)});
   });
+});
+
+//streznik poslusa
+app.listen(process.env.PORT, function() {
+  console.log("Strežnik posluša na portu " + process.env.PORT + ".");
 });
 
 
